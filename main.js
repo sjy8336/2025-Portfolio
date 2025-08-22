cb1353a396bae87422f9f8ade4b818fa46e99afe
sjy8336 <sjy8336@gmail.com>
2025-08-22 16:56:46 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var my = new fullpage('#wrap'/* , {
+    navigation: true,
+    responsiveWidth: 700,
+    anchors: ['home', 'profile', 'project', 'design', 'contact'],
+    parallax: true,
+    onLeave: function(origin, destination, direction){
+        console.log("Leaving section " + origin.index);
+}} */);
